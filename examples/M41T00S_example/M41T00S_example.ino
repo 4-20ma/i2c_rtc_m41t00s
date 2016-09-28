9024a717c80ab2b24f47280d6d6950478e0b3656
Doc Walker <4-20ma@wvfans.net>
2016-09-27 21:06:41 -0500

    Update license text in source, examples

--- examples/M41T00S_example/M41T00S_example.ino
+++ examples/M41T00S_example/M41T00S_example.ino
@@ -1,25 +1,24 @@
 /*
 
   M41T00S_example.ino - example using i2c_rtc_m41t00s library
-  
-  This file is part of i2c_rtc_m41t00s.
-  
-  i2c_rtc_m41t00s is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  i2c_rtc_m41t00s is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with i2c_rtc_m41t00s.  If not, see <http://www.gnu.org/licenses/>.
-  
-  Written by Doc Walker (Rx)
-  Copyright © 2009-2012 Doc Walker <4-20ma at wvfans dot net>
-  
+
+  Library:: i2c_rtc_m41t00s
+  Author:: Doc Walker <4-20ma@wvfans.net>
+
+  Copyright:: 2009-2016 Doc Walker
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
 */
 
 
